0baedf31b5f57560931e0bc80879f1640218863e
fedor-m <fedor-m@fedor-murashko>
2015-12-03 18:35:08 +0300

    testStart

--- scanning_system_2/src/main/java/ru/ang5545/scanning_system_2/App.java
+++ scanning_system_2/src/main/java/ru/ang5545/scanning_system_2/App.java
@@ -1,13 +1,8 @@
 package ru.ang5545.scanning_system_2;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+
+public class App {
+    public static void main( String[] args ){
+        System.out.println( "TestApp" );
     }
 }
