8eeef400963b49280e7897b4af3265d9c35ccaea
fedor-m <fedor-m@fedor-murashko>
2015-12-03 18:31:53 +0300

    first commit

---
+++ scanning_system_2/src/main/java/ru/ang5545/scanning_system_2/App.java
@@ -0,0 +1,13 @@
+package ru.ang5545.scanning_system_2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
